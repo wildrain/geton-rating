4846537709f0b0ad3830602b6de7249880b84630
Roman-Ul-Ferdosh <roman.ul.ferdosh@gmail.com>
2021-11-20 15:48:43 +0600

    add minimal css and fixing issues

--- assets/js/gor-rating.js
+++ assets/js/gor-rating.js
@@ -1,20 +1,18 @@
 (function ($) {
   const ajaxUrl = gor_data.ajax_url;
   let comments = gor_data.comments;
-  setStarRating();
-  function setStarRating() {
-    $('.gor-rating').starRating({
-      starSize: 25,
-      initialRating: 0,
-      disableAfterRate: false,
-      onHover: function (currentIndex, currentRating, $el) {
-        $('.live-rating').text(currentIndex);
-      },
-      onLeave: function (currentIndex, currentRating, $el) {
-        $('.live-rating').text(currentRating);
-      },
-    });
-  }
+
+  $('.gor-rating').starRating({
+    starSize: 25,
+    initialRating: 0,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+      $('.live-rating').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+      $('.live-rating').text(currentRating);
+    },
+  });
 
   $('.gor-avg-rating').starRating({
     readOnly: true,
@@ -34,15 +32,15 @@
   $('#geton-rating-form form').on('submit', function (event) {
     event.preventDefault();
 
-    let rating = $('.my-rating').starRating('getRating');
+    let rating = $('.gor-rating').starRating('getRating');
     let data = $(this).serialize();
-
     if (rating) {
       data = `${data}&rating=${rating}`;
     }
 
     $.post(gor_data.ajax_url, data, function (response) {
       console.log('response ', response);
+      location.reload();
     }).fail(function () {
       console.log('Something goes wrong');
     });
--- includes/templates/single-movie.php
+++ includes/templates/single-movie.php
@@ -1,50 +1,57 @@
-<?php get_header(); ?>
+<?php
+get_header();
 
-<div class="average-rating">
-    <span class="gor-avg-rating"></span>
-</div>
+global $post;
+$comment_details = geton_rating_get_comments($post->ID);
 
+$comments = $comment_details['comments'];
+$avg_rating = $comment_details['avg_rating'];
 
-<div class="geton-rating-form" id="geton-rating-form">
+?>
+<div id="gor-content" class="gor-content">
 
-    <?php
-    global $post;
-    $comments = geton_rating_get_comments($post->ID)['comments'];
-    ?>
+    <h2><?php echo get_the_title($post->ID); ?></h2>
+    <div class="average-rating">
+        <strong class="avg-rating-count"><?php echo $avg_rating; ?></strong>
+        <span class="gor-avg-rating"></span>
+    </div>
 
+    <h3><?php echo esc_html__('Reviews', 'gor-rating'); ?></h3>
     <ul class="gor-comments">
+
         <?php foreach ($comments as $comment) : ?>
             <li class="comment">
                 <?php if (!empty($comment['rating'])) : ?>
                     <span class="rating gor-rating-<?php echo $comment['comment_id']; ?>"></span>
-                    <span class="rating-count"><?php echo $comment['rating']; ?></span>
+                    <strong class="rating-count"><?php echo $comment['rating']; ?>,</strong>
                 <?php endif; ?>
                 <span class="review"><?php echo $comment['review']; ?></span>
             </li>
         <?php endforeach; ?>
     </ul>
 
-    <form action="" method="post">
+    <div class="geton-rating-form" id="geton-rating-form">
+        <form action="" method="post">
 
-        <span class="gor-rating"></span>
-        <span class="live-rating"></span>
+            <span class="gor-rating"></span>
+            <span class="live-rating"></span>
 
-        <div class="form-row">
-            <label for="review"><?php _e('Review', 'geton-rating'); ?></label>
-            <textarea name="review" id="review" required></textarea>
-        </div>
+            <div class="form-row">
+                <label for="review"><?php _e('Review', 'geton-rating'); ?></label>
+                <textarea name="review" id="review" required></textarea>
+            </div>
 
-        <div class="form-row">
-            <?php wp_nonce_field('geton-rating-form'); ?>
-            <input type="hidden" name="post_id" value="<?php echo $post->ID; ?>">
-            <input type="hidden" name="action" value="geton_rating_form">
-            <input type="submit" name="send_rating" value="<?php esc_attr_e('Send Review', 'geton-rating'); ?>">
-        </div>
+            <div class="form-row">
+                <?php wp_nonce_field('geton-rating-form'); ?>
+                <input type="hidden" name="post_id" value="<?php echo $post->ID; ?>">
+                <input type="hidden" name="action" value="geton_rating_form">
+                <input type="submit" name="send_rating" value="<?php esc_attr_e('Send Review', 'geton-rating'); ?>">
+            </div>
 
-    </form>
+        </form>
+    </div>
+
 </div>
-
-
 
 
 <?php get_footer(); ?>
